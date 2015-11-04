c9e62a58fb9d75e26c5e4edc67547066b40c05e4
lee10104 <lee95625@gmail.com>
2015-11-05 04:08:29 +0900

    sooo sleepyyyyy

--- hw5/translate.ml
+++ hw5/translate.ml
@@ -14,25 +14,25 @@ module Translator = struct
     | K.TRUE -> [Sm5.PUSH (Sm5.Val (Sm5.B true))]
     | K.FALSE -> [Sm5.PUSH (Sm5.Val (Sm5.B false))]
     | K.UNIT -> [Sm5.PUSH (Sm5.Val (Sm5.Unit))]
-    | K.VAR x ->
+    | K.VAR x -> raise (Error "Unimplemented")
     | K.ADD (e1, e2) -> trans e1 @ trans e2 @ [Sm5.ADD]
     | K.SUB (e1, e2) -> trans e1 @ trans e2 @ [Sm5.SUB]
     | K.MUL (e1, e2) -> trans e1 @ trans e2 @ [Sm5.MUL]
     | K.DIV (e1, e2) -> trans e1 @ trans e2 @ [Sm5.DIV]
-    | K.EQUAL (e1, e2) ->
-    | K.LESS (e1, e2) ->
-    | K.NOT e ->
-    | K.ASSIGN (x, e) ->
-    | K.SEQ (e1, e2) ->
-    | K.IF (e1, e2, e3) ->
-    | K.WHILE (e1, e2) ->
-    | K.FOR (x, e1, e2, e3) ->
+    | K.EQUAL (e1, e2) -> trans e1 @ trans e2 @ [Sm5.EQ]
+    | K.LESS (e1, e2) -> trans e1 @ trans e2 @ [Sm5.LESS]
+    | K.NOT e -> trans e @ [Sm5.NOT]
+    | K.ASSIGN (x, e) -> raise (Error "Unimplemented")
+    | K.SEQ (e1, e2) -> raise (Error "Unimplemented")
+    | K.IF (e1, e2, e3) -> raise (Error "Unimplemented")
+    | K.WHILE (e1, e2) -> raise (Error "Unimplemented")
+    | K.FOR (x, e1, e2, e3) -> raise (Error "Unimplemented")
     | K.LETV (x, e1, e2) ->
       trans e1 @ [Sm5.MALLOC; Sm5.BIND x; Sm5.PUSH (Sm5.Id x); Sm5.STORE] @
       trans e2 @ [Sm5.UNBIND; Sm5.POP]
-    | K.LETF (x, y, e1, e2) ->
-    | K.CALLV (x, e) ->
-    | K.CALLR (x, y) ->
+    | K.LETF (x, y, e1, e2) -> raise (Error "Unimplemented")
+    | K.CALLV (x, e) -> raise (Error "Unimplemented")
+    | K.CALLR (x, y) -> raise (Error "Unimplemented")
     | K.READ x -> [Sm5.GET; Sm5.PUSH (Sm5.Id x); Sm5.STORE; Sm5.PUSH (Sm5.Id x); Sm5.LOAD]
-    | K.WRITE e
+    | K.WRITE e -> raise (Error "Unimplemented")
 end
