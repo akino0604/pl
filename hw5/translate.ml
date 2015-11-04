944a1138e7cc4e7cd10496cfa53da9dbd14fc30d
lee10104 <lee95625@gmail.com>
2015-11-05 03:00:53 +0900

    sleepyyyy

--- hw5/translate.ml
+++ hw5/translate.ml
@@ -11,10 +11,28 @@ module Translator = struct
   (* TODO : complete this function  *)
   let rec trans : K.program -> Sm5.command = function
     | K.NUM i -> [Sm5.PUSH (Sm5.Val (Sm5.Z i))]
+    | K.TRUE -> [Sm5.PUSH (Sm5.Val (Sm5.B true))]
+    | K.FALSE -> [Sm5.PUSH (Sm5.Val (Sm5.B false))]
+    | K.UNIT -> [Sm5.PUSH (Sm5.Val (Sm5.Unit))]
+    | K.VAR x ->
     | K.ADD (e1, e2) -> trans e1 @ trans e2 @ [Sm5.ADD]
+    | K.SUB (e1, e2) -> trans e1 @ trans e2 @ [Sm5.SUB]
+    | K.MUL (e1, e2) -> trans e1 @ trans e2 @ [Sm5.MUL]
+    | K.DIV (e1, e2) -> trans e1 @ trans e2 @ [Sm5.DIV]
+    | K.EQUAL (e1, e2) ->
+    | K.LESS (e1, e2) ->
+    | K.NOT e ->
+    | K.ASSIGN (x, e) ->
+    | K.SEQ (e1, e2) ->
+    | K.IF (e1, e2, e3) ->
+    | K.WHILE (e1, e2) ->
+    | K.FOR (x, e1, e2, e3) ->
     | K.LETV (x, e1, e2) ->
       trans e1 @ [Sm5.MALLOC; Sm5.BIND x; Sm5.PUSH (Sm5.Id x); Sm5.STORE] @
       trans e2 @ [Sm5.UNBIND; Sm5.POP]
+    | K.LETF (x, y, e1, e2) ->
+    | K.CALLV (x, e) ->
+    | K.CALLR (x, y) ->
     | K.READ x -> [Sm5.GET; Sm5.PUSH (Sm5.Id x); Sm5.STORE; Sm5.PUSH (Sm5.Id x); Sm5.LOAD]
-    | _ -> failwith "Unimplemented"
+    | K.WRITE e
 end
