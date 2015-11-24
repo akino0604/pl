bca449209ba23703c29b39ec3fd4433d1700c04f
lee10104 <lee95625@gmail.com>
2015-11-24 11:07:10 +0900

    testcase 1, 2, 4 이상 통과

--- hw6/ex1/rozetta.ml
+++ hw6/ex1/rozetta.ml
@@ -15,15 +15,19 @@ let trans_v : Sm5.value -> Sonata.value = function
 let rec trans_obj : Sm5.obj -> Sonata.obj = function
   | Sm5.Val v -> Sonata.Val (trans_v v)
   | Sm5.Id id -> Sonata.Id id
-  | Sm5.Fn (arg, command) -> failwith "TODO : fill in here"
+  | Sm5.Fn (arg, command) -> Sonata.Fn (arg, trans' ([Sm5.BIND "last!"] @ command @ [Sm5.PUSH (Sm5.Id "last!"); Sm5.PUSH (Sm5.Val (Sm5.Z 0)); Sm5.MALLOC; Sm5.CALL]))
 
 (* TODO : complete this function *)
 and trans' : Sm5.command -> Sonata.command = function
-  | Sm5.PUSH obj :: cmds -> Sonata.PUSH (trans_obj obj) :: (trans' cmds)
+  | Sm5.PUSH obj :: cmds ->
+   (match obj with
+    | Sm5.Fn (arg, command) ->
+      Sonata.PUSH (trans_obj obj) :: Sonata.MALLOC :: Sonata.BIND arg :: Sonata.PUSH (Sonata.Id arg) :: Sonata.STORE :: Sonata.PUSH (trans_obj obj) :: (trans' cmds)
+    | _ -> Sonata.PUSH (trans_obj obj) :: (trans' cmds))
   | Sm5.POP :: cmds -> Sonata.POP :: (trans' cmds)
   | Sm5.STORE :: cmds -> Sonata.STORE :: (trans' cmds)
   | Sm5.LOAD :: cmds -> Sonata.LOAD :: (trans' cmds)
-  | Sm5.JTR (c1, c2) :: cmds ->  failwith "TODO : fill in here"
+  | Sm5.JTR (c1, c2) :: cmds -> [Sonata.JTR (trans' (c1 @ cmds), trans' (c2 @ cmds))]
   | Sm5.MALLOC :: cmds -> Sonata.MALLOC :: (trans' cmds)
   | Sm5.BOX z :: cmds -> Sonata.BOX z :: (trans' cmds)
   | Sm5.UNBOX id :: cmds -> Sonata.UNBOX id :: (trans' cmds)
@@ -31,7 +35,7 @@ and trans' : Sm5.command -> Sonata.command = function
   | Sm5.UNBIND :: cmds -> Sonata.UNBIND :: (trans' cmds)
   | Sm5.GET ::cmds -> Sonata.GET :: (trans' cmds)
   | Sm5.PUT ::cmds -> Sonata.PUT :: (trans' cmds)
-  | Sm5.CALL :: cmds -> failwith "TODO : fill in here"
+  | Sm5.CALL :: cmds -> Sonata.MALLOC :: Sonata.BIND "temploc!" :: Sonata.PUSH (Sonata.Id "temploc!") :: Sonata.STORE :: Sonata.MALLOC :: Sonata.BIND "tempval!" :: Sonata.PUSH (Sonata.Id "tempval!") :: Sonata.STORE :: Sonata.MALLOC :: Sonata.BIND "tempfun!" :: Sonata.PUSH (Sonata.Id "tempfun!") :: Sonata.STORE :: Sonata.PUSH (Sonata.Fn ("last!", trans' cmds)) :: Sonata.PUSH (Sonata.Id "tempfun!") :: Sonata.LOAD :: Sonata.PUSH (Sonata.Id "tempval!") :: Sonata.LOAD :: Sonata.PUSH (Sonata.Id "temploc!") :: Sonata.LOAD :: Sonata.CALL :: (trans' cmds) 
   | Sm5.ADD :: cmds -> Sonata.ADD :: (trans' cmds)
   | Sm5.SUB :: cmds -> Sonata.SUB :: (trans' cmds)
   | Sm5.MUL :: cmds -> Sonata.MUL :: (trans' cmds)
@@ -43,4 +47,4 @@ and trans' : Sm5.command -> Sonata.command = function
 
 (* TODO : complete this function *)
 let trans : Sm5.command -> Sonata.command = fun command ->
-  failwith "TODO : fill in here"
+  trans' command
